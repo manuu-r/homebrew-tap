be984001e53cf2bd284619eaf40e380ae19ec8bb
Manu <13869998+manuu-r@users.noreply.github.com>
2026-08-28 23:33:11 +0000

    Add bottles for 0.1.4

--- Formula/gauge.rb
+++ Formula/gauge.rb
@@ -6,6 +6,13 @@ class Gauge < Formula
   license "MIT"
   head "https://github.com/manuu-r/homebrew-tap.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/manuu-r/homebrew-tap/releases/download/v0.1.4"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "98c05e6c965210da6a3358d511d03f47bd88aec4b3b6d68ecb0c25d0673a9f59"
+    sha256 cellar: :any_skip_relocation, sequoia:       "fb05bab0bfbb1fc64c92fcdad1947484cb8ef8fd531d047085893fb87e3c20c2"
+  end
+
   depends_on "rust" => :build
   # Reads the macOS keychain via security(1) and drives the macOS menu bar.
   depends_on :macos
